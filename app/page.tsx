eda82c6578e7c39ba622f5f65705df93e26c9930
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-04-20 09:53:40 -0300

    refac: change blog path from /blog/path to /path

--- app/page.tsx
+++ app/page.tsx
@@ -43,10 +43,7 @@ export default async function Page() {
                       <div className='space-y-6'>
                         <div>
                           <h2 className='text-2xl font-bold leading-8 tracking-tight'>
-                            <Link
-                              href={`/blog/${slug}`}
-                              className='text-gray-900 dark:text-gray-100'
-                            >
+                            <Link href={`/${slug}`} className='text-gray-900 dark:text-gray-100'>
                               {title}
                             </Link>
                           </h2>
@@ -62,7 +59,7 @@ export default async function Page() {
                       </div>
                       <div className='text-base font-medium leading-6'>
                         <Link
-                          href={`/blog/${slug}`}
+                          href={`/${slug}`}
                           className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
                           aria-label={`Read more: "${title}"`}
                         >
