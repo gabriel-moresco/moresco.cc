6a938b9f9bea2c9c2a7b51d45de180ad63168ed0
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-04-20 09:07:51 -0300

    refac: remove projects page

--- data/header-links.ts
+++ data/header-links.ts
@@ -2,6 +2,5 @@ export const headerLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
