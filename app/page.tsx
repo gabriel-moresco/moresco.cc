ba69a11591f1995d1708018fa7369d99680da1c0
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-04-19 22:06:10 -0300

    refac: remove newsletter components

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import siteMetadata from '@/data/siteMetadata'
 import { Tag } from '@/components/Tag'
 import { formatDate } from 'pliny/utils/formatDate'
 import { Link } from '@/components/Link'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 const MAX_DISPLAY = 5
 
@@ -84,11 +83,6 @@ export default async function Page() {
           >
             All Posts &rarr;
           </Link>
-        </div>
-      )}
-      {siteMetadata.newsletter?.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
         </div>
       )}
     </>
