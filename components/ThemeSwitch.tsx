a71d3bd75c275e338bcacaf70d9a46bf6eb8da6b
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-04-19 21:59:57 -0300

    refac: move every svg icon to `icons.tsx`

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -3,49 +3,7 @@
 import { Fragment, useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
 import { Menu, RadioGroup, Transition } from '@headlessui/react'
-
-const Sun = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    className="h-6 w-6 text-gray-900 dark:text-gray-100"
-  >
-    <path
-      fillRule="evenodd"
-      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-      clipRule="evenodd"
-    />
-  </svg>
-)
-
-const Moon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    className="h-6 w-6 text-gray-900 dark:text-gray-100"
-  >
-    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-  </svg>
-)
-
-const Monitor = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="h-6 w-6 text-gray-900 dark:text-gray-100"
-  >
-    <rect x="3" y="3" width="14" height="10" rx="2" ry="2"></rect>
-    <line x1="7" y1="17" x2="13" y2="17"></line>
-    <line x1="10" y1="13" x2="10" y2="17"></line>
-  </svg>
-)
+import { Monitor, Moon, Sun } from '@/data/icons'
 
 export const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
@@ -58,7 +16,13 @@ export const ThemeSwitch = () => {
     <div className="mr-5">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button>{resolvedTheme === 'dark' ? <Moon /> : <Sun />}</Menu.Button>
+          <Menu.Button>
+            {resolvedTheme === 'dark' ? (
+              <Moon className="h-6 w-6 text-gray-900 dark:text-gray-100" />
+            ) : (
+              <Sun className="h-6 w-6 text-gray-900 dark:text-gray-100" />
+            )}
+          </Menu.Button>
         </div>
         <Transition
           as={Fragment}
@@ -76,7 +40,7 @@ export const ThemeSwitch = () => {
                   <Menu.Item>
                     <button className="group flex w-full items-center rounded-md px-2 py-2 text-sm">
                       <div className="mr-2">
-                        <Sun />
+                        <Sun className="h-6 w-6 text-gray-900 dark:text-gray-100" />
                       </div>
                       Light
                     </button>
@@ -86,7 +50,7 @@ export const ThemeSwitch = () => {
                   <Menu.Item>
                     <button className="group flex w-full items-center rounded-md px-2 py-2 text-sm">
                       <div className="mr-2">
-                        <Moon />
+                        <Moon className="h-6 w-6 text-gray-900 dark:text-gray-100" />
                       </div>
                       Dark
                     </button>
@@ -96,7 +60,7 @@ export const ThemeSwitch = () => {
                   <Menu.Item>
                     <button className="group flex w-full items-center rounded-md px-2 py-2 text-sm">
                       <div className="mr-2">
-                        <Monitor />
+                        <Monitor className="h-6 w-6 text-gray-900 dark:text-gray-100" />
                       </div>
                       System
                     </button>
