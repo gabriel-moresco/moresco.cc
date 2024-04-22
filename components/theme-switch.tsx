2387f8ac2e139789130790b00e747fbc460a1657
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-04-22 00:17:16 -0300

    feat: change theme-switch to display icon depending on tailwind theme

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,18 +1,11 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-
 import { useTheme } from 'next-themes'
 
 import { Moon, Sun } from '@/components/icons'
 
 export const ThemeSwitch = () => {
-  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
 
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
@@ -20,11 +13,8 @@ export const ThemeSwitch = () => {
 
   return (
     <button className='mr-5' onClick={toggleTheme}>
-      {theme === 'dark' || !mounted ? (
-        <Moon className='h-6 w-6 text-gray-900 dark:text-gray-100' />
-      ) : (
-        <Sun className='h-6 w-6 text-gray-900 dark:text-gray-100' />
-      )}
+      <Moon className='hidden h-6 w-6 text-gray-900 dark:flex dark:text-gray-100' />
+      <Sun className='flex h-6 w-6 text-gray-900 dark:hidden dark:text-gray-100' />
     </button>
   )
 }
