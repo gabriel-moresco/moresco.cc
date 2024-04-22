fca2f7137d6f6e980cbbbed4972e03f94402c1fd
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-04-22 19:31:52 -0300

    feat: change home h1 to Gabriel Moresco

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default async function Page() {
   return (
     <div className='divide-y divide-gray-200 dark:divide-gray-700'>
       <div className='space-y-2 pb-8 pt-6 md:space-y-5'>
-        <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
-          Latest
+        <h1 className='text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14'>
+          Gabriel Moresco
         </h1>
 
         <p className='text-lg leading-7 text-gray-500 dark:text-gray-400'>
